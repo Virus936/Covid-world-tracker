c8f9b71767ed466f3233d1b2015d97bbc9e484fa
Virus936 <lok.vira@gmail.com>
2020-08-03 23:05:00 +0200

    fix issues : le selecteur de pays n'etait pas dans le bon ordre

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import StatByCountry from "./components/StatByCountry";
 import "leaflet/dist/leaflet.css";
 import "./App.css";
 
-
 function App() {
   const [country, setCountry] = useState("worldwide");
   const [countries, setCountries] = useState([]);
@@ -16,55 +15,60 @@ function App() {
   const [covidState, setCovidState] = useState("deaths");
 
   const getCountriesList = async () => {
-    const listcountry = await fetch( "https://disease.sh/v3/covid-19/countries")
-      .then(function(res){
-        const data = res.json()
-        data.then(value=> setCountriesInfo(value))
-        return data
-      })
-      .then(data =>
-        data.map(country => ({
+    await fetch("https://disease.sh/v3/covid-19/countries")
+      .then(res => res.json())
+      .then(data => {
+        const countries = data.map(country => ({
           name: country.country,
           value: country.countryInfo.iso2
-        }))
-      );
-    setCountries(listcountry);
+        }));
+        setCountriesInfo(data);
+        setCountries(countries);
+      });
   };
 
-  const getCountriesCovidData = async (url) => {
-    const data = await fetch(url)
-      .then(res=> res.json())
-    setCountryInfo(data)
-  }
+  const getCountriesCovidData = async url => {
+    const data = await fetch(url).then(res => res.json());
+    setCountryInfo(data);
+  };
 
-  const handleClick = (covidcase) => {
-    setCovidState(covidcase)
-  }
+  const handleClick = covidcase => {
+    setCovidState(covidcase);
+  };
 
   useEffect(() => {
     getCountriesList();
   }, []);
 
   useEffect(() => {
-    const url = country === 'worldwide' ? "https://disease.sh/v3/covid-19/all" : `https://disease.sh/v3/covid-19/countries/${ country }`
-    getCountriesCovidData(url)
+    const url =
+      country === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${country}`;
+    getCountriesCovidData(url);
   }, [country]);
 
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value)
-  }
+  const onChangeCountry = e => {
+    setCountry(e.target.value);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>COVID19-Tracker</h1>
-        <SelectCountry 
-          country={country} 
-          countries = {countries} 
-          onChangeCountry={onChangeCountry} />
+        <SelectCountry
+          country={country}
+          countries={countries}
+          onChangeCountry={onChangeCountry}
+        />
       </header>
-      <CovidStat handleClick={handleClick} covidstate={covidState} countryInfo={countryInfo} />
+      <CovidStat
+        handleClick={handleClick}
+        covidstate={covidState}
+        countryInfo={countryInfo}
+      />
       <Map country={countryInfo} />
-      <StatByCountry countriesInfo = {countriesInfo}/>
+      <StatByCountry countriesInfo={countriesInfo} />
     </div>
   );
 }
